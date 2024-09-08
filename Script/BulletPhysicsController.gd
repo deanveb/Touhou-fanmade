9ddb956717394ae32fda8cc7747c3ea02adfda34
deanveb <116471433+deanveb@users.noreply.github.com>
2024-09-08 11:14:56 +0700

    Update Bullet Physics Controller
    Bullet Physics Controller can be used with animation player

--- Script/BulletPhysicsController.gd
+++ Script/BulletPhysicsController.gd
@@ -1,12 +1,18 @@
 extends Node2D
 
+@export_category("nodes")
 @export var bullet : CharacterBody2D
+@export var animation : AnimationPlayer
+@export_category("varibles")
 @export var speed : int
 @export var radius : float
+@export var start_speed : int
+@export var end_speed : int
+@export var mid_speed : int
 
 func _ready() -> void:
-	var collision : CollisionShape2D = bullet.get_node("CollisionShape2D") as CollisionShape2D
-	collision.position.y += radius
+	if animation:
+		animation.play("movement")
 
 func _process(delta: float) -> void:
 	bullet.velocity = Vector2(cos(bullet.rotation), sin(bullet.rotation)) * speed

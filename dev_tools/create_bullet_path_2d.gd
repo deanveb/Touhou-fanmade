810ab2d09a531555a7a83df75a1f96c3732e69bb
deanveb <116471433+deanveb@users.noreply.github.com>
2024-09-14 14:23:04 +0700

    Added dev_tool folder
    -Bullet patterns now can be drawed with equations(only circle is supported)

---
+++ dev_tools/create_bullet_path_2d.gd
@@ -0,0 +1,36 @@
+extends Node2D
+
+@export var zoom : int
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	#TODO: loop twice to get y1, y2 
+	#TODO: make path2d point at the center
+	
+	var file : FileAccess = FileAccess.open("res://dev_tools/get_coord_path2d/output.csv", FileAccess.READ)
+	var bullet_pattern : Path2D = Path2D.new() as Path2D
+	var header : Array = file.get_csv_line()
+	var coords : Array
+	
+	while !file.eof_reached():
+		var data : Array = file.get_csv_line()
+		if data[0] == "":
+			continue
+		var coord : Dictionary
+		for i in range(len(data)):
+			coord[header[i]] = data[i]
+		coords.append(coord)
+	
+	var curve : Curve2D = Curve2D.new()
+	for coord : Dictionary in coords:
+		var point : Vector2 = Vector2(float(coord["x"]), float(coord["y1"]))
+		curve.add_point(point)
+	for i in range(coords.size() - 1, -1, -1):
+		if len(coords[i]) != 3:
+			continue
+		var point : Vector2 = Vector2(float(coords[i]["x"]), float(coords[i]["y2"]))
+		curve.add_point(point)
+	curve.add_point(Vector2(float(coords[0]["x"]), float(coords[0]["y1"])))
+	bullet_pattern.curve = curve
+	bullet_pattern.scale = Vector2(1, 1) * zoom
+	add_child(bullet_pattern)
---
+++ dev_tools/get_coord_path2d/convert.py
@@ -0,0 +1,77 @@
+from sympy.solvers import solve
+from sympy import Symbol, N, Float
+from math import sqrt
+from csv import writer
+
+y = Symbol('y')
+
+def main():
+    # TODO: promts input for gap and add a bat file
+    # Fix Cases with no a,b
+    equations = []
+    with open("input.txt", 'r') as f:
+        equation = f.readline()
+        while equation:
+            equations.append(clean_up(equation)) 
+            equation = f.readline()
+        
+    datas = []
+    for equation in equations:
+        coords = []
+        if "^{2" in equation:
+            coords = get_circle_coord(equation, 1, 1)
+            for coord in coords:
+                    if len(coord) == 3 and coord[1] < coord[2]:
+                        coord[1], coord[2] = coord[2], coord[1]
+        else:
+            pass
+        datas.append(coords)
+
+    with open("output.csv", "w", newline='') as table:
+        write = writer(table)
+        # Add header
+        write.writerow(['x', 'y1', 'y2'])
+        for data in datas:
+            for value in data:
+                write.writerow(value)
+            write.writerow([])
+
+def get_circle_coord(equation, multiplier, gap):
+    num = ""
+    recordable = False
+    # 1 = a, 2 = b, 3 = R
+    # Get Value
+    varnames = []
+    for i in range(len(equation)):
+        dig = equation[i]
+        if (dig.isdigit() or dig == "-") and equation[i - 1] != '{':
+            num += dig
+            recordable = True
+        elif not dig.isdigit() and recordable:
+            varnames.append(num)
+            num = ""
+            recordable = False
+    for i in range(len(varnames) - 1):
+        varnames[i] = int(varnames[i]) * -1
+    # Process varibles
+    varnames[2] = int(varnames[2])
+    R = int(sqrt(varnames[2]))
+    results = []
+    for x in range(varnames[0] - R, varnames[0] + R + 1, gap):
+        results.append([x] + solve((x - varnames[0])**2 + (y - varnames[1])**2 - varnames[2], y))
+    # solve((gap - int(varnames[0]) * -1)**2 + (y - int(varnames[1]) * -1)**2 - int(varnames[2]), y)
+    for result in results:
+        for j in range(len(result)):
+            result[j] = Float(N(result[j] * multiplier), 5)
+    return results
+
+def clean_up(equation):
+    # Remove unnecessary characters
+    equation = equation.replace(' ', '')
+    equation = equation.replace('\\', '')
+    # To also check the last num
+    equation += ' '
+    return equation
+
+if __name__ == "__main__":
+    main()
